7b5060bf8ffcefeacea0549ac5072fc0cff745ea
¡Duque <atanaelleonardo@gmail.com>
2024-04-14 18:29:12 -0300

    feat(remove rules for header capitalization and period at end): remove rules for header
    This commit removes the rules for ensuring that the commit message header starts with a capitalized
    letter and ends with a period.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,8 +3,8 @@ module.exports = {
   extends: [],
   rules: {
     'header-min-length': [2, 'always', 20],
-    'header-case-start-capital': [2, 'always'],
-    'header-end-period': [2, 'always'],
+    // 'header-case-start-capital': [2, 'always'],
+    // 'header-end-period': [2, 'always'],
   },
   plugins: [
     {
